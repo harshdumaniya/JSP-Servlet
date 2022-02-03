ddbdba674ce63cce8caae6131b613ef617f96dc8
hd972392 <harshdumaniya972392@gmail.com>
2022-02-03 19:48:02 +0530

    DAO Class

--- Registration-Form-JSP-Servlet/src/com/registration/dao/EmployeeDAO.java
+++ Registration-Form-JSP-Servlet/src/com/registration/dao/EmployeeDAO.java
@@ -9,23 +9,20 @@ import com.registration.util.DatabaseConnection;
 public class EmployeeDAO {
 
 	public int registerEmployee(Employee employee) throws ClassNotFoundException {
-		Connection connection;
-		PreparedStatement preparedStatement;
-
 		try {
-			connection = DatabaseConnection.connection();
-		String INSERT_USERS_SQL = "INSERT INTO employee"
+			Connection connection = DatabaseConnection.connection();
+			String INSERT_USERS_SQL = "INSERT INTO employee"
 				+ "  (first_name, last_name, username, password, address, contact) VALUES "
 				+ " (?, ?, ?, ?,?,?);";
 
-				preparedStatement = connection.prepareStatement(INSERT_USERS_SQL);
-				preparedStatement.setString(1, employee.getFirstName());
-				preparedStatement.setString(2, employee.getLastName());
-				preparedStatement.setString(3, employee.getUsername());
-				preparedStatement.setString(4, employee.getPassword());
-				preparedStatement.setString(5, employee.getAddress());
-				preparedStatement.setString(6, employee.getContactNo());
-				preparedStatement.executeUpdate();
+			PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL);
+			preparedStatement.setString(1, employee.getFirstName());
+			preparedStatement.setString(2, employee.getLastName());
+			preparedStatement.setString(3, employee.getUsername());
+			preparedStatement.setString(4, employee.getPassword());
+			preparedStatement.setString(5, employee.getAddress());
+			preparedStatement.setString(6, employee.getContactNo());
+			preparedStatement.executeUpdate();
 		} catch (SQLException e) {
 			  e.printStackTrace();
 		}
